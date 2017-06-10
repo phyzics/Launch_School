dd66abc7f10b00bcc99f806386843aab8e889be0
Nick Calibey <nick.calibey@gmail.com>
2017-06-10 10:37:41 -0500

    updated rock_paper_scissors_lizard_spock.rb logic

--- 101_programming_foundations/lesson_2/rock_paper_scissors_lizard_spock.rb
+++ 101_programming_foundations/lesson_2/rock_paper_scissors_lizard_spock.rb
@@ -1,69 +1,103 @@
-VALID_CHOICES = %w(rock paper scissors lizard spock)
+VALID_CHOICES = {'r'  => 'rock',
+                 'p'  => 'paper',
+                 'sc' => 'scissors',
+                 'l'  => 'lizard',
+                 'sp' => 'spock'
+                }
 
-=begin
-def win?(first, second)
-  (first == 'rock' && second == 'scissors')   ||
-    (first == 'paper' && second == 'rock')    ||
-    (first == 'scissors' && second == 'paper')
-end
-=end
-
-def prompt(message)
-  puts "=> #{message}"
-end
-
-def win?(key, player1, player2)
-  # convert string to symbol
-  # look up values based upon symbol
-  # if player 2 input is there, you win
-  key[player1.to_sym].include?(player2)
-end
-
-def display_results(key, player, computer)
-  if win?(key, player, computer)
-    prompt('You won!')
-  elsif win?(key, computer, player)
-    prompt('Computer won!')
-  else
-    prompt("It's a tie!")
-  end
-end
-
-# player makes a choice
-# if value contained in key, player wins
-
-
-
-victory_key = {rock: ['scissors', 'lizard'],
+VICTORY_KEY = {rock: ['scissors', 'lizard'],
                paper: ['rock', 'spock'],
                scissors: ['paper', 'lizard'],
                lizard: ['paper', 'spock'],
                spock: ['rock', 'scissors']
               }
 
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def win?(player1, player2)
+  VICTORY_KEY[player1.to_sym].include?(player2)
+end
+
+def display_results(player1, player2)
+  if win?(player1, player2)
+    prompt('You won!')
+    return 'player'
+  elsif win?(player2, player1)
+    prompt('Computer won!')
+    return 'computer'
+  else
+    prompt("It's a tie!")
+  end
+end
+
+def clear_screen
+  system('clear') || system('cls')
+end
+
+clear_screen
+
+player_score = 0
+computer_score = 0
 
 loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp.downcase
+  until player_score == 5 || computer_score == 5 do
+    choice = ''
+    loop do
+      prompt("Choose one: ")
+      choice = gets.chomp.downcase
 
-    if VALID_CHOICES.include?(choice)
+      if VALID_CHOICES.has_key?(choice) || VALID_CHOICES.has_value?(choice)
+        choice = VALID_CHOICES[choice] if VALID_CHOICES.has_key?(choice)
+        break
+        # add a 'see options' prompt
+      elsif choice == 'exit'
+        prompt("Thanks for playing. Good bye!")
+        exit
+      else
+        prompt("That's not a valid choice.")
+      end
+    end
+
+    clear_screen
+
+    computer_choice = VALID_CHOICES.values.sample
+
+    prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+    perform_round = display_results(choice, computer_choice)
+
+    player_score += 1 if perform_round == 'player'
+    computer_score += 1 if perform_round == 'computer'
+
+    puts player_score
+    puts computer_score
+  end
+
+  if player_score == 5
+    puts "Congratulations! You are the GRAND CHAMPION!"
+    puts "----------------------------------".center(44)
+  else
+    puts "Oh no! The Computer is the GRAND CHAMPION!".center(40)
+    puts "----------------------------------"
+  end
+
+  prompt("Would you like to play another set? (Enter 'y' or 'n')")
+  new_set = ''
+  loop do
+    new_set = gets.chomp.downcase
+    if new_set.downcase == 'y'
+      player_score = 0
+      computer_score = 0
+      break
+    elsif new_set.downcase == 'n'
       break
     else
-      prompt("That's not a valid choice.")
+      prompt("Please enter either 'y' or 'n'.")
     end
   end
 
-  computer_choice = VALID_CHOICES.sample
-
-  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
-
-  display_results(victory_key, choice, computer_choice)
-
-  prompt('Do you want to play again?')
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  break unless new_set.downcase == 'y'
 end
-
-prompt('Thank you for playing. Good bye!')
+prompt('Thanks for playing. Good bye!')
