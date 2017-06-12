dfa9d19ba270307439a8e969b9567adc3a56dac1
Nick Calibey <nick.calibey@gmail.com>
2017-06-11 20:16:59 -0500

    added easy2.rb (incomplete)

---
+++ 101_programming_foundations/lesson_3/easy2.rb
@@ -0,0 +1,37 @@
+# easy2.rb
+
+=begin
+=end
+# 1
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+ages.include?("Spot")
+ages.key?("Spot")
+ages.has_key?("Spot")
+ages.fetch("Spot", "It ain't here, brudduh")
+
+# 2
+munsters_description = "The Munsters are creepy in a good way."
+
+munsters_description.capitalize!
+munsters_description.swapcase!
+munsters_description.downcase!
+munsters_description.upcase!
+
+# 3
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+additional_ages = { "Marilyn" => 22, "Spot" => 237 }
+ages.merge!(additional_ages)
+
+# 4
+advice = "Few things in life are as important as house training your pet dinosaur."
+advice.include?("Dino")
+
+# 5
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+# 6
+flintstones << "Dino"
+
+#7
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones.insert(-1, 'Dino', 'Hoppy')
