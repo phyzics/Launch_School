145d340f25fb5d904028c2cfe90180879f3653a0
Nick Calibey <nick.calibey@gmail.com>
2017-06-14 17:10:18 -0500

    updated hard1.rb answer #4

--- 101_programming_foundations/lesson_3/hard1.rb
+++ 101_programming_foundations/lesson_3/hard1.rb
@@ -29,10 +29,5 @@ C)
 def dot_separated_ip_address?(input_string)
   dot_separated_words = input_string.split(".")
   return false if dot_separated_words.size != 4
-
-  dot_separated_words.each do |number|
-    return false unless is_an_ip_number?(number)
-  end
-
-  true
+  dot_separated_words.all? { |number| is_an_ip_number?(number) }
 end
