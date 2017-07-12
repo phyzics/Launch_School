08cf2ef7ed09a6fc2f4b7acc53d5bd968785fe43
Nick Calibey <Nick@Nick-Calibeys-MacBook-Pro.local>
2017-07-12 12:54:40 -0500

    added more programming

--- 101_programming_foundations/lesson_6/twenty_one.rb
+++ 101_programming_foundations/lesson_6/twenty_one.rb
@@ -1,6 +1,81 @@
 #twenty_one.rb
+require 'pry'
 
+FACE_NAMES = {
+  'A' => 'Ace',
+  'J' => 'Jack',
+  'Q' => 'Queen',
+  'K' => 'King'
+}
 
+SUITS = {
+  'S' => 'Spades',
+  'D' => 'Diamonds',
+  'C' => 'Clubs',
+  'H' => 'Hearts'
+}
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+def busted?(hand)
+  if calculate_total(hand) > 21
+    return true
+  else
+    return false
+  end
+end
+
+def calculate_total(hand)
+  values = hand.map { |card| card[1] }
+  
+  hand_total = 0
+  values.each do |card|
+    if card == 'A'
+      hand_total += 11
+    elsif card.to_i == 0
+      hand_total += 10
+    else
+      hand_total += card.to_i
+    end
+  end
+  
+  values.select { |value| value == 'A' }.count.times do
+    hand_total -=10 if hand_total > 21
+  end
+  
+  hand_total
+  #binding.pry
+end
+
+def decide_victor(player1, player2)
+  return true if player1 > player2
+  return false if player2 > player1
+end
+
+def display_hand(player, hand)
+  display_array = hand.map do |card|
+  	if card[1].to_i.to_s == card[1]
+  	  "#{card[1]} of #{SUITS[card[0]]}"
+  	else
+  	  "#{FACE_NAMES[card[1]]} of #{SUITS[card[0]]}"
+  	end
+  end
+  display_string = join_array(display_array)
+  display_string = "#{player} has #{display_string}"
+  #binding.pry
+end
+
+def join_array(arr)
+  case arr.size
+  when 2
+    arr.join(" and ")
+  else
+    arr[-1] = "and #{arr[-1]}"
+    arr.join(", ")
+  end
+end
 
 deck = [['H', '2'], ['H', '3'], ['H', '4'], ['H', '5'], ['H', '6'], ['H', '7'],
         ['H', '8'], ['H', '9'], ['H', '10'],['H', 'J'], ['H', 'Q'], ['H', 'K'],
@@ -18,17 +93,48 @@ dealers_hand = []
 2.times { players_hand << deck.sample }
 2.times { dealers_hand << deck.sample }
 
-p players_hand
+prompt(display_hand("Player", players_hand))
+prompt(display_hand("Dealer", dealers_hand))
+calculate_total(players_hand)
+
+
 
 answer = nil
 loop do
-  puts "hit or stay?"
-  answer = gets.chomp
-  break if answer == 'stay' || busted?
+  puts ""
+  prompt('Hit or Stay?')
+  answer = gets.chomp.downcase
+  
+  case answer
+  when 'hit'
+    players_hand << deck.sample
+    prompt(display_hand("Player", players_hand))  
+  when 'stay'
+    break
+  else
+    prompt("Please select either 'Hit' or 'Stay'") 
+  end
+  
+  break if busted?(players_hand)
+  
 end
 
-if busted?
-  # play again?
+if busted?(players_hand)
+  prompt ("Busted!")# play again?
 else
-  puts "You chose to stay!"
+  prompt("You chose to stay!")
 end
+
+loop do
+  case dealers_hand
+  when calculate_total(dealers_hand) < 17
+    prompt("Dealer hits!")
+    dealers_hand << deck.sample
+  when calculate_total(dealers_hand) >= 17
+    prompt("Dealer stays!")
+    break
+  end
+  
+  break if busted?(dealers_hand)
+end
+# judge victor
