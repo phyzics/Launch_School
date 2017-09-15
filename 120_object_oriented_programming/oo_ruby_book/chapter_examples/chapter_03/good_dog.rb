41ac1e65813e61c9a449461a51558fc7d1435a4c
Nick Calibey <nick.calibey@gmail.com>
2017-09-15 00:10:37 -0500

    added chapter_03 dir and corresponding good_dog.rb file

---
+++ 120_object_oriented_programming/oo_ruby_book/chapter_examples/chapter_03/good_dog.rb
@@ -0,0 +1,31 @@
+# good_dog.rb
+
+class GoodDog
+  attr_accessor :name, :height, :weight
+
+  def initialize(n, h, w)
+    self.name   = n
+    self.height = h
+    self.weight = w
+  end
+
+  def change_info(n, h, w)
+    self.name   = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{self.name} weights #{self.weight} and is #{self.height} tall."
+  end
+
+  def what_is_self
+    self
+  end
+
+  puts self
+end
+
+sparky = GoodDog.new('Sparky', '12 inches', '10 lbs')
+p sparky.what_is_self
+puts GoodDog
