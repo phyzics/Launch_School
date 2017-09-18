740b1c7265fbdc30bd8b10d6e946f2f101f8645f
Nick Calibey <nick.calibey@gmail.com>
2017-09-18 12:39:02 -0500

    added both zitcode ruby tutorial files

---
+++ 120_object_oriented_programming/lesson_01/zetcode_02.rb
@@ -0,0 +1,18 @@
+# zetcode_02.rb - Second Zetcode OOP Tutorial
+class BigValueError < StandardError ; end
+
+LIMIT = 333
+x = 3_432_453
+
+begin
+  if x > LIMIT
+    raise BigValueError, "Exceeded the maximum value"
+  end
+
+  puts "Script continues"
+
+rescue => e
+  puts e
+  p e
+  exit 1
+end
