622d3922a76f4bed5cbc3cef4b86c9e09c2c35e4
Nick Calibey <nick.calibey@gmail.com>
2017-09-19 18:50:31 -0500

    updated oo_rps.rb to comply with rubocop

--- 120_object_oriented_programming/lesson_02/oo_rps/oo_rps.rb
+++ 120_object_oriented_programming/lesson_02/oo_rps/oo_rps.rb
@@ -19,29 +19,15 @@ class Move
   end
 
   def >(other_move)
-    if rock?
-      return true if other_move.scissors?
-      return false
-    elsif paper?
-      return true if other_move.rock?
-      return false
-    elsif scissors?
-      return true if other_move.paper?
-      return false
-    end
+    (rock? && other_move.scissors?)   ||
+      (paper? && other_move.rock?)    ||
+      (scissors? && other_move.paper?)
   end
 
   def <(other_move)
-    if rock?
-      return true if other_move.paper?
-      return false
-    elsif paper?
-      return true if other_move.scissors?
-      return false
-    elsif scissors?
-      return true if other_move.rock?
-      return false
-    end
+    (rock? && other_move.paper?)       ||
+      (paper? && other_move.scissors?) ||
+      (scissors? && other_move.rock?)
   end
 
   def to_s
@@ -108,10 +94,12 @@ class RPSGame
     puts "thanks for playing Rock, Paper, Scissors. Good bye!"
   end
 
-  def display_winner
+  def display_moves
     puts "#{human.name} chose #{human.move}."
     puts "#{computer.name} chose #{computer.move}."
+  end
 
+  def display_winner
     if human.move > computer.move
       puts "#{human.name} won!"
     elsif human.move < computer.move
@@ -130,7 +118,7 @@ class RPSGame
       puts "Sorry, must be 'y' or 'n'."
     end
 
-    return true if answer == 'y'
+    return true if answer.downcase == 'y'
     false
   end
 
@@ -139,6 +127,7 @@ class RPSGame
     loop do
       human.choose
       computer.choose
+      display_moves
       display_winner
       break unless play_again?
     end
