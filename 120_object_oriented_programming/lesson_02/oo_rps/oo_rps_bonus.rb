d22974f359f09700e3556954cf11f2f919f21ba0
Nick Calibey <nick.calibey@gmail.com>
2017-09-19 22:10:05 -0500

    added oo_rps_bonus.rb and implemented score tracking feature

---
+++ 120_object_oriented_programming/lesson_02/oo_rps/oo_rps_bonus.rb
@@ -0,0 +1,175 @@
+# oo_rps.rb - Object Oriented Rock, Paper, Scisssors
+class Move
+  VALUES = ['rock', 'paper', 'scissors']
+
+  def initialize(value)
+    @value = value
+  end
+
+  def scissors?
+    @value == 'scissors'
+  end
+
+  def rock?
+    @value == 'rock'
+  end
+
+  def paper?
+    @value == 'paper'
+  end
+
+  def >(other_move)
+    (rock? && other_move.scissors?)   ||
+      (paper? && other_move.rock?)    ||
+      (scissors? && other_move.paper?)
+  end
+
+  def <(other_move)
+    (rock? && other_move.paper?)       ||
+      (paper? && other_move.scissors?) ||
+      (scissors? && other_move.rock?)
+  end
+
+  def to_s
+    @value
+  end
+end
+
+class Player
+  attr_accessor :move, :name, :score
+
+  def initialize
+    set_name
+  end
+
+  def reset_score
+    self.score = 0
+  end
+end
+
+class Human < Player
+  def set_name
+    n = ''
+    puts "What's your name?"
+    loop do
+      n = gets.chomp
+      break unless n.empty?
+      puts "Sorry, must enter a value."
+    end
+    self.name = n
+  end
+
+  def choose
+    choice = nil
+    loop do
+      puts "Please choose rock, paper, or scissors"
+      choice = gets.chomp
+      break if Move::VALUES.include?(choice)
+      puts "Sorry, invalid choice."
+    end
+    self.move = Move.new(choice)
+  end
+end
+
+class Computer < Player
+  def set_name
+    self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
+  end
+
+  def choose
+    self.move = Move.new(Move::VALUES.sample)
+  end
+end
+
+# Game Orchestration Engine
+class RPSGame
+  attr_accessor :human, :computer
+
+  def initialize
+    @human = Human.new
+    @computer = Computer.new
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors!"
+  end
+
+  def display_goodbye_message
+    puts "thanks for playing Rock, Paper, Scissors. Good bye!"
+  end
+
+  def display_moves
+    puts "#{human.name} chose #{human.move}."
+    puts "#{computer.name} chose #{computer.move}."
+  end
+
+  def determine_winner
+    if human.move > computer.move
+      :human
+    elsif human.move < computer.move
+      :computer
+    else
+      :tie
+    end
+  end
+
+  def display_winner
+    case determine_winner
+    when :human then puts "#{human.name} won!"
+    when :computer then puts "#{computer.name} won!"
+    when :tie then puts "It's a tie!"
+    end
+  end
+
+  def iterate_scores
+    case determine_winner
+    when :human then human.score += 1
+    when :computer then computer.score += 1
+    end
+  end
+
+  def display_scores
+    puts "The current score is:"
+    puts "#{human.name}: #{human.score}"
+    puts "#{computer.name}: #{computer.score}"
+  end
+
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp
+      break if ['y', 'n'].include?(answer.downcase)
+      puts "Sorry, must be 'y' or 'n'."
+    end
+
+    return true if answer.downcase == 'y'
+    false
+  end
+
+  def reset_scores
+    human.reset_score
+    computer.reset_score
+  end
+
+  def play
+    display_welcome_message
+    loop do
+      reset_scores
+      loop do
+        human.choose
+        computer.choose
+        display_moves
+        determine_winner
+        display_winner
+        iterate_scores
+        display_scores
+        break if (human.score || computer.score) == 3
+      end
+      break unless play_again?
+    end
+    display_goodbye_message
+  end
+end
+
+RPSGame.new.play
