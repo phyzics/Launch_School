b0ecefdbfbe600c1db45d8b857378d8b06dfb5fd
Nick Calibey <nick.calibey@gmail.com>
2017-09-22 19:31:55 -0500

    added easy_02 dir with easy_201.rb

---
+++ 120_object_oriented_programming/lesson_03/easy_02/easy_201.rb
@@ -0,0 +1,16 @@
+# easy_201.rb
+class Oracle
+  def predict_the_future
+    "You will " + choices.sample
+  end
+
+  def choices
+    ["eat a nice lunch", "take a nap soon", "stay at work late"]
+  end
+end
+
+oracle = Oracle.new
+p oracle.predict_the_future
+
+# it will return a new string with one of the three strings from the array
+# returned by `choices`
