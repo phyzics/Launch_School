8cb59446886f9aa80ee81593eded3ddf06796469
Nick Calibey <nick.calibey@gmail.com>
2017-10-10 10:55:30 -0500

    added todolist.rb

---
+++ 130_ruby_foundations/lesson_01/todolist.rb
@@ -0,0 +1,122 @@
+# todolist.rb
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+end
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  def <<(todo)
+    raise TypeError.new("Can only add Todo objects") if todo.class != Todo
+
+    todos << todo
+  end
+  alias_method :add, :<<
+
+  def size
+    todos.size
+  end
+
+  def first
+    todos.first
+  end
+
+  def last
+    todos.last
+  end
+
+  def item_at(idx)
+    todos.fetch(idx)
+  end
+
+  def mark_done_at(idx)
+    item_at(idx).done!
+  end
+
+  def mark_undone_at(idx)
+    item_at(idx).undone!
+  end
+
+  def shift
+    todos.shift
+  end
+
+  def pop
+    todos.pop
+  end
+
+  def remove_at(idx)
+    todos.delete(item_at(idx))
+  end
+
+  def to_s
+    str = "---- #{title} ----\n"
+    str << todos.map(&:to_s).join("\n")
+    str
+  end
+
+  def done!
+    todos.each do |item|
+      item.done!
+    end
+  end
+
+  def done?
+    todos.all?(&:done?)
+  end
+
+  private
+
+  attr_reader :todos
+end
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+list = TodoList.new("Today's Todos")
+
+list.add(todo1)
+list.add(todo2)
+list << todo3
+
+list.size
+list.first
+list.last
+
+list.item_at(1)
+list.mark_done_at(1)
+list.mark_undone_at(1)
+
+list.done!
+p list.done?
+puts list
