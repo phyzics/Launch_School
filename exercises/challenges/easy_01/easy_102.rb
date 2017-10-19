aa173686294845a66e30da38ddafb61c2146f813
Nick Calibey <nick.calibey@gmail.com>
2017-10-19 09:45:17 -0500

    fixed solution so it accurately modeled algorithm

--- exercises/challenges/easy_01/easy_102.rb
+++ exercises/challenges/easy_01/easy_102.rb
@@ -1,21 +1,14 @@
 # easy_102.rb
 class Sieve
-  attr_reader :limit, :range_arr
+  attr_reader :range_arr
 
   def initialize(limit)
-    @limit = limit
     @range_arr = (2..limit).to_a
   end
 
   def primes
-    multiples_arr = []
     range_arr.each do |num|
-      next if multiples_arr.include?(num)
-      range_arr.each do |n|
-        next if num == n || multiples_arr.include?(n)
-        multiples_arr << n if n % num == 0
-      end
+      range_arr.reject! { |n| n % num == 0 && n != num }
     end
-    range_arr - multiples_arr
   end
 end
