6976251183214a14daf680468dbcb1e78e1ef597
Nick Calibey <nick.calibey@gmail.com>
2017-11-09 22:46:47 -0600

    added my_framework rack app

---
+++ 170_web_development/lesson_04/my_framework/app.rb
@@ -0,0 +1,24 @@
+# app.rb
+
+require_relative 'monroe'
+require_relative 'advice'
+
+class App < Monroe
+  def call(env)
+    case env['REQUEST_PATH']
+    when '/'
+      status = '200'
+      headers = {'Content-Type' => 'text/html'}
+      response(status, headers) { erb(:index) }
+    when '/advice'
+      piece_of_advice = Advice.new.generate
+      status = '200'
+      headers = {"Content-Type" => 'text/html'}
+      response(status, headers) { erb(:advice, message: piece_of_advice) }
+    else
+      status = '404'
+      headers = {"Content-Type" => 'text/html', "Content-Length" => '84'}
+      response(status, headers) { erb(:not_found) }
+    end
+  end
+end
