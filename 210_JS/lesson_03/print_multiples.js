a70bc26d2428c503603b6d0a8c41db506ee5aa94
Nick Calibey <nick.calibey@gmail.com>
2018-04-09 19:58:33 -0500

    added print_multiples.js assignment

--- 210_JS/lesson_03/print_multiples.js
+++ 210_JS/lesson_03/print_multiples.js
@@ -14,5 +14,15 @@ function logMultiples(number) {
 // Further Exploration
 
 function logMultiples(number) {
+  if (number % 2 === 0) {
+    return;
+  }
+
   largestMultiple = Math.floor(100 / number) * number;
+
+  for (var i = largestMultiple; i >= number; i -= number) {
+    if (i % 2 !== 0) {
+      console.log(i);
+    }
+  }
 }
