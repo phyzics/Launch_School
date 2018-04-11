e34c65ea809afaabaac60ddb3c42da52345b4995
Nick Calibey <nick.calibey@gmail.com>
2018-04-10 22:14:44 -0500

    added password.js

---
+++ 210_JS/lesson_03/password.js
@@ -0,0 +1,13 @@
+// password.js
+var failedAttempts = 0;
+var input;
+
+while (failedAttempts < 3) {
+  input = prompt('What is the password: ');
+
+  if (input === 'password') {
+    break;
+  }
+}
+
+input === 'password' ? console.log('You have successfully logged in.') : console.log('You have been denied access.');
