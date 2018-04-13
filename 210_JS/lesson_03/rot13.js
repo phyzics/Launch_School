aab902f41b71dcf0d4c4cfaecf0e90c9f9659c21
Nick Calibey <nick.calibey@gmail.com>
2018-04-13 02:17:16 -0500

    refactored solution

--- 210_JS/lesson_03/rot13.js
+++ 210_JS/lesson_03/rot13.js
@@ -1,20 +1,28 @@
 // rot13.js - Rot13 Cipher
 function rot13(string) {
-  var CONVERSION_OFFSET = 13;
   var newString = '';
   var i;
 
   for (i = 0; i < string.length; i++) {
-    charCode = string.charCodeAt(i);
-
-    if ((string[i] >= 'A' && string[i] <= 'M') || (string[i] >= 'a' && string[i] <= 'm')) {
-      charCode += CONVERSION_OFFSET;
-    } else if ((string[i] >= 'N' && string[i] <= 'Z') || (string[i] >= 'n' && string[i] <= 'z')) {
-      charCode -= CONVERSION_OFFSET;
-    }
-
-    newString += String.fromCharCode(charCode);
+    newChar = rot13Character(string[i]);
+    newString += newChar;
   }
 
   return newString;
 }
+
+function rot13Character(char) {
+  var CONVERSION_OFFSET = 13;
+  var charCode = char.charCodeAt(0);
+
+  if (char.match(/[a-m]/i)) {
+    charCode += CONVERSION_OFFSET;
+  } else if (char.match(/[n-z]/i)) {
+    charCode -= CONVERSION_OFFSET;
+  }
+
+  return String.fromCharCode(charCode);
+}
+
+console.log(rot13('Teachers open the door, but you must enter by yourself.'));
+console.log(rot13(rot13('Teachers open the door, but you must enter by yourself.')));
