bb63776ef22a6a17385e05885589b866aa74a285
Nick Calibey <nick.calibey@gmail.com>
2018-04-19 09:46:16 -0500

    finished 'shift and unshift' exercise

--- exercises/JS_fundamentals/arrays/arrays_107.js
+++ exercises/JS_fundamentals/arrays/arrays_107.js
@@ -14,16 +14,47 @@ function shift(array) {
 
 function unshift(array) {
   var i;
-  var j;
+  var initialLength = array.length;
   var totalLength = 0;
+  var arrCopy = array.slice();
 
   for (i = 0; i < arguments.length; i++) {
-    totalLength += arguments[i].length;
+    if (i === 0) {
+      totalLength += arguments[i].length;
+    } else {
+      totalLength += 1;
+    }
   }
 
   array.length = totalLength;
 
-  for (i = 0; i < arguments.length; i++) {
-    
+  for (i = 0; i < initialLength; i++) {
+    array[array.length - 1 - i] = arrCopy[arrCopy.length - 1 - i];
   }
+
+  for (i = 1; i < arguments.length; i++) {
+    array[i - 1] = arguments[i];
+  }
+
+  return array.length;
+}
+
+// Launch School's Solutions
+function shift(array) {
+  var result;
+
+  if (array.length !==0) {
+    result = array.splice(0, 1).pop();
+  }
+
+  return result;
+}
+
+function unshift(array) {
+  var i;
+  for (i = 1; i < arguments.length; i++) {
+    array.splice(i - 1, 0, arguments[i]);
+  }
+
+  return array.length;
 }
