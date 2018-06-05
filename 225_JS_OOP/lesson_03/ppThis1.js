ce762908b1025b5f7ece34127b3d57eeb543b453
Nick Calibey <nick.calibey@gmail.com>
2018-06-05 11:36:02 -0500

    added first  exercise

---
+++ 225_JS_OOP/lesson_03/ppThis1.js
@@ -0,0 +1,18 @@
+// ppThis1.js - Practice Problems: What is this? (1)
+/*
+1) `window` object [incorrect]
+    - we don't know what `this` points to until execution time
+
+2) the `window` object
+
+3) the `window` object
+
+4) `obj`
+
+5) 2
+
+6) 2
+
+7) `foo` <-- Object {a: 1} (but yes, the obj foo)
+   `window` <-- wrong: throws a TypeError as this.baz is not defined at global level
+*/
