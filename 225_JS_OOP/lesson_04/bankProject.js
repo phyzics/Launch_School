af5475ce99cbb47b421c68a9a9098a32f9d12d0e
Nick Calibey <nick.calibey@gmail.com>
2018-06-06 11:35:51 -0500

    finished 'Banking with Objects and Closures' project

--- 225_JS_OOP/lesson_04/bankProject.js
+++ 225_JS_OOP/lesson_04/bankProject.js
@@ -1,1 +1,48 @@
 // bankProject.js - Project: Banking with Objects and Closures
+function makeBank() {
+  function makeAccount(number) {
+    var balance = 0;
+    var transactions = [];
+
+    return {
+      balance: function() { return balance; },
+
+      deposit: function(amount) {
+        transactions.push({ type: 'deposit', amount: amount });
+        return balance += amount;
+      },
+
+      number: function() { return number; },
+
+      transactions: function() { return transactions; },
+
+      withdraw: function(amount) {
+        if (amount > balance) amount = balance;
+
+        transactions.push({ type: 'withdraw', amount: amount });
+        balance -= amount;
+        return amount;
+      },
+    }
+  }
+
+  var accounts = [];
+
+  return {
+    openAccount: function() {
+      var number = accounts.length + 101;
+      var newAccount = makeAccount(number);
+      accounts.push(newAccount);
+      return newAccount;
+    },
+
+    transfer: function(source, destination, amount) {
+      source.withdraw(amount);
+      destination.deposit(amount);
+      return amount;
+    }
+  }
+}
+
+var bank = makeBank();
+console.log(bank.accounts);
