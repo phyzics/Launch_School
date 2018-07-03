9a24e40ca7c40e12e131d8a73d77e381af891fd6
Nick Calibey <nick.calibey@gmail.com>
2018-07-03 17:49:36 -0500

    finished 'Search Filtering' project

---
+++ 230_DOM_Async/lesson_04/search_filtering/javascripts/search.js
@@ -0,0 +1,52 @@
+$(function () {
+  var CATALOG = [{
+    "title": "The Legend of Zelda: Majora's Mask 3D",
+    "id": 1,
+    "category": "Nintendo 3DS"
+  }, {
+    "title": "Super Smash Bros.",
+    "id": 2,
+    "category": "Nintendo 3DS"
+  }, {
+    "title": "Super Smash Bros.",
+    "id": 3,
+    "category": "Nintendo WiiU"
+  }, {
+    "title": "LEGO Batman 3: Beyond Gotham",
+    "id": 4,
+    "category": "Nintendo WiiU"
+  }, {
+    "title": "LEGO Batman 3: Beyond Gotham",
+    "id": 5,
+    "category": "Xbox One"
+  }, {
+    "title": "LEGO Batman 3: Beyond Gotham",
+    "id": 6,
+    "category": "PlayStation 4"
+  }, {
+    "title": "Far Cry 4",
+    "id": 7,
+    "category": "PlayStation 4"
+  }, {
+    "title": "Far Cry 4",
+    "id": 8,
+    "category": "Xbox One"
+  }, {
+    "title": "Call of Duty: Advanced Warfare",
+    "id": 9,
+    "category": "PlayStation 4"
+  }, {
+    "title": "Call of Duty: Advanced Warfare",
+    "id": 10,
+    "category": "Xbox One"
+  }];
+
+  $('aside form').on('click', 'input', function (e) {
+    var input = e.currentTarget.value;
+    var ids = CATALOG.filter(function (game) {
+      return game.category === input;
+    }).map(function (game) { return game.id; });
+
+    ids.forEach(function (id) { $('[data-id=' + String(id) + ']').toggle(); });
+  });
+});
